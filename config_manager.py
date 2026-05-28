d8ff4f201673de331bb942c63968eb700e415bd0
Jjjjkooo <thomas.jjjjkooo@gmail.com>
2026-05-28 21:44:23 +0800

    Added topmost function for chat window

--- config_manager.py
+++ config_manager.py
@@ -184,6 +184,7 @@ DEFAULTS = {
     "pinned_chat_keys": [],
     "fluent_chat_window_enabled": True,
     "chat_window_normal_window": False,
+    "chat_window_always_on_top": False,
     "group_chat_sidebar_ratio": 0.28,
     "group_chat_sidebar_collapsed": False,
     "pov_mode": "off",
