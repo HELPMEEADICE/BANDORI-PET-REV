46d65a5e6ec91eeccd9ce7daa9a845e364d186f2
HELPMEEADICE <yuanzhenpeng210@gmail.com>
2026-07-07 04:05:07 +0800

    更新q群链接

--- app_info.py
+++ app_info.py
@@ -4,6 +4,6 @@ APP_VERSION = "3.1.3"
 APP_REPOSITORY = "HELPMEEADICE/BANDORI-PET-REV"
 APP_REPO_URL = f"https://github.com/{APP_REPOSITORY}"
 APP_LICENSE_URL = f"{APP_REPO_URL}/blob/main/LICENSE"
-APP_QQ_GROUP_URL = "https://qm.qq.com/q/VJMrn5EkWQ"
+APP_QQ_GROUP_URL = "https://qm.qq.com/q/jkFnvpmWJi"
 
 MAIN_EXECUTABLE = "BandoriPet.exe"
